267b8841f2628d7ea0c9f86a94cf6ec186435466
Dylan Butman <dbutman@gmail.com>
2017-01-23 18:37:09 +0100

    update build

--- build.clj
+++ build.clj
@@ -1,7 +1,24 @@
-(ns build
-  (:require [clojure.edn :as edn]
-            [clojure.java.io :as io]
-            [clojure.string :as str]))
+(require '[clojure.java.io :as io])
+(require '[clojure.java.shell :as sh])
+(require '[clojure.string :as str])
+
+(defn next-version [version]
+  (when version
+    (let [[a b] (next (re-matches #"(.*?)([\d]+)" version))]
+      (when (and a b)
+        (str a (inc (Long/parseLong b)))))))
+
+(defn deduce-version-from-git
+  "Avoid another decade of pointless, unnecessary and error-prone
+  fiddling with version labels in source code."
+  []
+  (let [[version commits hash dirty?]
+        (next (re-matches #"(.*?)-(.*?)-(.*?)(-dirty)?\n"
+                          (:out (sh/sh "git" "describe" "--dirty" "--long" "--tags"))))]
+    (cond
+      dirty? (str (next-version version) "-" hash "-dirty")
+      (pos? (Long/parseLong commits)) (str (next-version version) "-" hash)
+      :otherwise version)))
 
 (defn join-keys
   [ks]
@@ -10,11 +27,18 @@
        (str/join "-")
        keyword))
 
-(defn korks->ks
-  [kork]
-  (if (keyword? kork)
-    (vector kork)
-    kork))
+(defn expr->ks
+  [expr]
+  (cond
+    (keyword? expr) [[expr]]
+    (map? expr) (mapcat identity
+                        (for [[k vals] expr]
+                          (mapv (partial vector k) vals)))
+    (vector? expr) expr))
+
+(defn pull->ks
+  [expr]
+  (mapcat expr->ks expr))
 
 (defn flatten-vals
   "takes a hashmap and recursively returns a flattened list of all the values"
@@ -23,15 +47,15 @@
     coll
     (mapcat flatten-vals (vals coll))))
 
-(defn select-deps [deps korks]
-  (->> korks
-       (mapv (comp (partial get-in deps)
-                   korks->ks))
-       (mapcat flatten-vals)
-       (into [])))
-
-(def deps
-  '(edn/read-string (slurp "deps.edn")))
+(defn pull-deps
+  ([deps expr] (pull-deps deps nil expr))
+  ([deps scope expr]
+   (cond->> (->> expr
+                 pull->ks
+                 (mapv (partial get-in deps))
+                 (mapcat flatten-vals)
+                 (into []))
+     scope (mapv #(conj % :scope scope)))))
 
 (set-env!
  :repositories #(conj % ["my.datomic.com" {:url "https://my.datomic.com/repo"
@@ -43,24 +67,59 @@
                                        :cljs-repl
                                        :cljs-test
                                        :laces
-                                       :reload]}]
-                              (select-deps deps)
-                              (mapv #(conj % :scope "test"))))))
+                                       :reload
+                                       :test
+                                       :garden
+                                       :http
+                                       :beanstalk]}
+                               :env
+                               :fs]
+                              (pull-deps deps "test")))))
 
 (require
  '[boot.file :as file]
  '[boot.util :as util]
  '[clojure.java.io :as io]
  '[adzerk.bootlaces :refer :all]
+ '[adzerk.boot-beanstalk :refer [beanstalk dockerrun]]
  '[adzerk.boot-cljs :refer [cljs]]
  '[adzerk.boot-cljs-repl :refer [cljs-repl start-repl]]
  '[adzerk.boot-reload :refer [reload]]
  '[adzerk.boot-test :refer [test]]
  '[crisptrutski.boot-cljs-test :as ct]
- '[boot-component.reloaded :refer :all]
+ ;;'[boot-component.reloaded :refer :all]
  '[environ.core :refer [env]]
  '[org.martinklepsch.boot-garden :refer :all]
- '[pandeiro.boot-http :refer [serve]])
+ '[pandeiro.boot-http :refer [serve]]
+ '[me.raynes.fs :as fs])
+
+(def version (deduce-version-from-git))
+(bootlaces! version)
+
+(task-options!
+ pom {:project project
+      :version version
+      :license {"Eclipse Public License" "http://www.eclipse.org/legal/epl-v10.html"}
+      :url (str "https://github.com/pleasetrythisathome/" project)
+      :scm {:url (str "https://github.com/pleasetrythisathome/" project)}}
+ aot {:namespace #{'(symbol (str project ".main"))}}
+ jar {:main (symbol (str project ".main"))
+      :file (str project ".jar")}
+ ct/test-cljs {:namespaces [(symbol (str project ".main-test"))]}
+ garden {:output-to "public/css/style.css"
+         :vendors ["webkit" "moz" "o"]}
+ beanstalk {:name        project
+            :version     version
+            :description ""
+            :access-key  (env :aws-access-key-id)
+            :secret-key  (env :aws-secret-key)
+            :file        "target/project.zip"
+            :stack-name  "64bit Amazon Linux 2015.09 v2.0.7 running Docker 1.9.1"
+            :bucket      (str project "-deploy")
+            :beanstalk-envs (for [env ["dev" "staging" "prod"]
+                                  :let [name (str project "-" env)]]
+                              {:name name
+                               :cname-prefix name})})
 
 (deftask test-clj
   "test cljs"
@@ -88,8 +147,12 @@
 (deftask dev
   "watch and compile css, cljs, init cljs-repl and push changes to browser"
   []
-  (set-env! :source-paths   #(conj % "test" "dev"))
-  (set-env! :resource-paths #(conj % "test" "src"))
+  (set-env! :source-paths
+            #(reduce (fn [paths dir]
+                       (if (fs/exists? dir)
+                         (conj paths dir)
+                         paths))
+                     % ["test" "dev"]))
   (comp
    (watch)
    (notify)
@@ -133,7 +196,7 @@
   []
   (comp
    (add-repo)
-   (add-file :path "target/borderless.jar")
+   (add-file :path (str "target/" project ".jar"))
    (dockerrun)
    (zip)
    (target)))
