879521f5062415e131705e2c178ca0c174c4de89
Dylan Butman <dbutman@gmail.com>
2017-01-24 16:33:40 +0100

    remove void tasks

--- build.clj
+++ build.clj
@@ -1,6 +1,8 @@
-(require '[clojure.java.io :as io])
-(require '[clojure.java.shell :as sh])
-(require '[clojure.string :as str])
+(require '[boot.file :as file]
+         '[boot.util :as util]
+         '[clojure.java.io :as io]
+         '[clojure.java.shell :as sh]
+         '[clojure.string :as str])
 
 (defn next-version [version]
   (when version
@@ -63,120 +65,20 @@
                                            :password (get-sys-env "DATOMIC_PASS")}])
  :dependencies #(vec
                  (concat %
-                         (->> [{:boot [:cljs
-                                       :cljs-repl
-                                       :cljs-test
-                                       :laces
-                                       :reload
-                                       :test
-                                       :garden
-                                       :http
-                                       :beanstalk]}
-                               :env
-                               :fs]
-                              (pull-deps deps "test"))))
- :source-paths #{"src"})
+                         (->> [{:boot [:laces]}]
+                              (pull-deps deps "test")))))
 
 (require
- '[boot.file :as file]
- '[boot.util :as util]
- '[clojure.java.io :as io]
- '[adzerk.bootlaces :refer :all]
- '[adzerk.boot-beanstalk :refer [beanstalk dockerrun]]
- '[adzerk.boot-cljs :refer [cljs]]
- '[adzerk.boot-cljs-repl :refer [cljs-repl start-repl]]
- '[adzerk.boot-reload :refer [reload]]
- '[adzerk.boot-test :refer [test]]
- '[crisptrutski.boot-cljs-test :as ct]
- ;;'[boot-component.reloaded :refer :all]
- '[environ.core :refer [env]]
- '[org.martinklepsch.boot-garden :refer :all]
- '[pandeiro.boot-http :refer [serve]]
- '[me.raynes.fs :as fs])
+ '[adzerk.bootlaces :refer :all])
 
 (def version (deduce-version-from-git))
+
 (bootlaces! version)
 
-(task-options!
- pom {:project (symbol project)
-      :version version
-      :license {"Eclipse Public License" "http://www.eclipse.org/legal/epl-v10.html"}
-      :url (str "https://github.com/pleasetrythisathome/" project)
-      :scm {:url (str "https://github.com/pleasetrythisathome/" project)}}
- aot {:namespace #{'(symbol (str project ".main"))}}
- jar {:main (symbol (str project ".main"))
-      :file (str project ".jar")}
- ct/test-cljs {:namespaces [(symbol (str project ".main-test"))]}
- garden {:output-to "public/css/style.css"
-         :vendors ["webkit" "moz" "o"]}
- beanstalk {:name        project
-            :version     version
-            :description ""
-            :access-key  (env :aws-access-key-id)
-            :secret-key  (env :aws-secret-key)
-            :file        "target/project.zip"
-            :stack-name  "64bit Amazon Linux 2016.09 v2.4.0 running Docker 1.12.6"
-            :bucket      (str project "-deploy")
-            :beanstalk-envs (for [env ["dev" "staging" "prod"]
-                                  :let [name (str project "-" env)]]
-                              {:name name
-                               :cname-prefix name})})
-
-(deftask test-clj
-  "test cljs"
+(deftask show-version
+  "Show version"
   []
-  (set-env! :source-paths #(conj % "test"))
-  (comp
-   (test)))
-
-(deftask test-cljs
-  "test cljs"
-  []
-  (set-env! :source-paths #(conj % "test"))
-  (comp
-   (ct/prep-cljs-tests)
-   (cljs :source-map true)
-   (ct/run-cljs-tests)))
-
-(deftask test-all
-  "test all"
-  []
-  (comp
-   (test-clj)
-   (test-cljs)))
-
-(deftask dev
-  "watch and compile css, cljs, init cljs-repl and push changes to browser"
-  []
-  (set-env! :source-paths
-            #(reduce (fn [paths dir]
-                       (if (fs/exists? dir)
-                         (conj paths dir)
-                         paths))
-                     % ["test" "dev"]))
-  (apply clojure.tools.namespace.repl/set-refresh-dirs (get-env :directories))
-  (comp
-   (watch)
-   (notify)
-   (reload :port 3459
-           :ip "0.0.0.0"
-           :asset-host "/")
-   (cljs-repl :port 3458)
-   (cljs :source-map true)
-   (garden :pretty-print true)
-   (target)))
-
-(deftask package
-  "compile cljx, garden, cljs, and build a jar"
-  []
-  (comp
-   (cljs :optimizations :advanced)
-   (garden)
-   (aot)
-   (pom)
-   (uber)
-   (jar)
-   (target)))
+  (println version))
 
 (deftask add-file
   "add deployment files to fileset"
@@ -193,17 +95,9 @@
       @add-files
       (-> fileset (add-resource tgt) commit!))))
 
-(deftask build-docker
-  "Build my application docker zip file."
+(deftask nrepl
+  "start a nrepl server"
   []
   (comp
-   (add-repo)
-   (add-file :path (str "target/" project ".jar"))
-   (dockerrun)
-   (zip)
-   (target)))
-
-(deftask show-version
-  "Show version"
-  []
-  (println version))
+   (repl :server true)
+   (watch)))
